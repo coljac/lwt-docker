19d6f48fa61d7376bc688a41e840991086d26605
Colin Jacobs <colin@coljac.net>
2020-09-11 19:20:47 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lwt-docker
-Docker image for Learning with Texts
+
+Docker image for [Learning with Texts](http://lwt.sourceforge.net/).
+
+`docker run -p 8000:80 coljac/lwt:latest` gives you LWT on http://localhost:8000/lwt .
+
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/bin/bash -c "/usr/bin/mysqld_safe > /dev/null &"                                                                
+sleep 5                                                                                                          
+mysql -u debian-sys-maint -p$(cat pw) < script.sql
+
